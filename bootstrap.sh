52b738b2fd3ba01824ed0ffc74825685668d797d
root <root@ip-172-31-38-50.us-west-2.compute.internal>
2018-03-01 00:24:10 +0000

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+
+/bin/echo  "Configure yum repos ..."
+	/usr/bin/sudo yum-config-manager --enable epel
+	/usr/bin/sudo yum clean all
+/bin/echo  "Done"
+
+/bin/echo "Install ansible ..."
+	/usr/bin/sudo yum install -y ansible libselinux-python python2 && /bin/echo 'Install of ansible - done'
+/bin/echo "Done"
+
+/bin/echo "Run ansible to provision configuration ..."
+	/usr/bin/sudo /usr/bin/python2.6 /usr/bin/ansible-playbook ./ansible/site.yml --connection=local
+/bin/echo "Done"
