f8bf38276751b97b9b3efbd9ba1691d72b4c54f6
root <root@ip-172-31-36-147.us-west-2.compute.internal>
2018-03-01 19:23:07 +0000

    Fixes of playbooks and provisioning script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,16 @@
 	/usr/bin/sudo yum clean all
 /bin/echo  "Done"
 
-/bin/echo "Install ansible ..."
-	/usr/bin/sudo yum install -y ansible libselinux-python python2 && /bin/echo 'Install of ansible - done'
+/bin/echo "Install ansible for python2.6"
+	/usr/bin/sudo yum install -y ansible libselinux-python python2 git && /bin/echo 'Install of ansible and git - done'
+/bin/echo "Done"
+
+/bin/echo "Install ansible for pythong2.7"
+        /usr/bin/sudo pip install ansible && /bin/echo 'Install of ansible - done'
 /bin/echo "Done"
 
 /bin/echo "Run ansible to provision configuration ..."
+	/bin/mkdir -p /tmp/ansible_provisioning && cd /tmp/ansible_provisioning
+	/usr/bin/git clone https://github.com/gergund/packer-ansible-template.git . 
 	/usr/bin/sudo /usr/bin/python27 /usr/bin/ansible-playbook ./ansible/site.yml --connection=local
 /bin/echo "Done"
