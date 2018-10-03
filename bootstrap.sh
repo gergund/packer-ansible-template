36a799f4334eb0b4aaa947b850f77fa48c23835d
Denys Gergun <gergund@gmail.com>
2018-10-03 14:57:07 +0300

    cleaned and documented, updated AMI source image

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,19 @@
-
+#!/bin/bash
 /bin/echo  "Configure yum repos ..."
 	/usr/bin/sudo yum-config-manager --enable epel
 	/usr/bin/sudo yum clean all
 /bin/echo  "Done"
 
-/bin/echo "Install ansible for python2.6"
-	/usr/bin/sudo yum install -y ansible libselinux-python python2 git && /bin/echo 'Install of ansible and git - done'
+/bin/echo "Install yum packages"
+	/usr/bin/sudo yum install -y git && /bin/echo 'Install yum packages - done'
 /bin/echo "Done"
 
-/bin/echo "Install ansible for pythong2.7"
+/bin/echo "Install ansible"
         /usr/bin/sudo pip install ansible && /bin/echo 'Install of ansible - done'
 /bin/echo "Done"
 
 /bin/echo "Run ansible to provision configuration ..."
 	/bin/mkdir -p /tmp/ansible_provisioning && cd /tmp/ansible_provisioning
 	/usr/bin/git clone https://github.com/gergund/packer-ansible-template.git . 
-	/usr/bin/sudo /usr/bin/python27 /usr/bin/ansible-playbook ./ansible/site.yml --connection=local
+	/usr/bin/sudo /usr/local/bin/ansible-playbook ./ansible/site.yml --connection=local
 /bin/echo "Done"
