af8e1d7e1244a642c5bb0baf4038f9ceec1f02ed
Denys Gergun <gergund@gmail.com>
2018-10-04 10:31:26 +0300

    Fixes for vhost and deploy.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/bin/echo  "Configure yum repos ..."
+/bin/echo  "Configure yum repos"
 	/usr/bin/sudo yum-config-manager --enable epel
 	/usr/bin/sudo yum clean all
 /bin/echo  "YUM repos configuration - done"
@@ -9,10 +9,10 @@
 /bin/echo 'Install yum packages - done'
 
 /bin/echo "Install ansible"
-        /usr/bin/sudo pip install ansible
+        /usr/bin/sudo pip install ansible==2.6.5
 /bin/echo 'Install of ansible - done'
 
-/bin/echo "Run ansible to provision configuration ..."
+/bin/echo "Run ansible to provision configuration"
 	/bin/mkdir -p /tmp/ansible_provisioning && cd /tmp/ansible_provisioning
 	/usr/bin/git clone https://github.com/gergund/packer-ansible-template.git . 
 	/usr/bin/sudo /usr/local/bin/ansible-playbook ./ansible/site.yml --connection=local
@@ -20,4 +20,4 @@
 
 /bin/echo "Code deployment"
 	/bin/bash deploy.sh
-/bin/echo "Done"
+/bin/echo "Code deployment - done"
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir -p /var/www/html && cd /var/www/html/ && sudo git clone https://github.com/magento/magento2.git . 
+sudo mkdir -p /var/www/html && cd /var/www/html/ && sudo git clone https://github.com/magento/magento2.git . 
